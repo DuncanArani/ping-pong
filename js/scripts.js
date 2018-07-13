ee63c35c8c9fc889486b48f693068269a68e8da9
ARUNCO <duncanarani@gmail.com>
2018-07-13 14:17:46 +0300

    js changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
-$c(document).ready(function(){
+$(document).ready(function(){
   $("form#exp").submit(function(event){
     event.preventDefault();
-    var divisible=parseIn($(input#number))
+    var divisible=parseIn($(input#Number).var());
+    var output=pingpong(divisible)
   });
 });
+var pingpong=function(divisible){
+  for(x=1;x<=input;x++){
+    if(x%15===0){
+  var li=<li>+x+</li>
+}else{ (x%3===0){
+  var ul=<li>+x+</li>
+}else{ (x%5===0){
+ return input
+}
+}
+}
